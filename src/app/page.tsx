dcef9e4d62a020bbad1c1777d7dc89c36aabb3e4
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-11-29 13:44:41 -0300

    style: fix eslint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">
+      <h1 className="text-3xl font-black text-violet-700">
         Welcome to the world of Next.js
       </h1>
     </div>
