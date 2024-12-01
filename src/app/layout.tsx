3e6ff851ba0e3246ac64be8821df2ac5bb6452e5
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-01 18:18:49 -0300

    feat(navigation): create navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import React from "react";
 
 import "./globals.css";
+import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
@@ -42,6 +43,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
+
           {children}
         </ThemeProvider>
       </body>
