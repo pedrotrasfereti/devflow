af567bbb9316e73ccfa924cf043642fa2e2f6bd7
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-01-12 20:34:09 -0300

    refactor: organize pages in route groups

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+import Navbar from "@/components/navigation/navbar";
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <Navbar />
+
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
