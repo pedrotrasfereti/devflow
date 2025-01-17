b2e79fc4902861eaf1fd957a0ceabf0f46c86838
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-01-17 17:50:44 -0300

    feat(nav): combine logo and title (navbar)

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -8,18 +8,18 @@ import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-200 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href="/" className="flex items-center gap-2">
         <Image
           src="/images/site-logo.svg"
           width={23}
           height={23}
           alt="DevFlow Logo"
         />
-      </Link>
 
-      <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-        Dev<span className="text-primary-500">Flow</span>
-      </p>
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
 
       <p>Global Search</p>
 
