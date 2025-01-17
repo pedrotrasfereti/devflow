c0d06561b6cf6d81e27c2d73fce1fa625f33926f
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-01-17 17:06:28 -0300

    fix(MobileNavigation): unknown tailwind class

--- src/components/navigation/navbar/MobileNavigation.tsx
+++ src/components/navigation/navbar/MobileNavigation.tsx
@@ -48,7 +48,7 @@ const MobileNavigation = () => {
             </p>
           </Link>
 
-          <div className="not-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
+          <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
               <section className="flex h-full flex-col gap-6 pt-16">
                 <NavLinks />
