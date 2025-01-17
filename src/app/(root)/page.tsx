800c2941760cd5b5be815f66163343a798c1c51c
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-01-16 22:03:53 -0300

    refactor(homepage): remove example log off btn

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import ROUTES from "@/constants/routes";
-
-import { auth, signOut } from "../../../auth";
+import { auth } from "../../../auth";
 
 const Home = async () => {
   const session = await auth();
@@ -12,17 +10,6 @@ const Home = async () => {
       <h1 className="text-3xl font-black text-violet-700">
         Welcome to the world of Next.js
       </h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <button type="submit">Log out</button>
-      </form>
     </div>
   );
 };
