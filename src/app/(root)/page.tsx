0a0f926e78a383b1626160bd4c18f0fca9083924
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-01-24 23:01:55 -0300

    feat: add home page structure

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,33 @@
-import { auth } from "../../../auth";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
-
   return (
-    <div>
-      <h1 className="text-3xl font-black text-violet-700">
-        Welcome to the world of Next.js
-      </h1>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-11">LocalSearch</section>
+
+      <p>HomeFilter</p>
+
+      <section className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </section>
+    </>
   );
 };
 
