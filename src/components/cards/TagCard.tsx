d4b7470f1296635e5e041873f92a7a55c40ed974
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-11 13:25:29 -0300

    feat: add TagCard component variants
    Adds "compact" and "isButton" UI variants as well as close functionality to TagCard component.

--- src/components/cards/TagCard.tsx
+++ src/components/cards/TagCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -9,33 +10,72 @@ import { Badge } from "../ui/badge";
 interface Props {
   _id: string;
   name: string;
-  count: number;
+  count?: number;
   showCount?: boolean;
   compact?: boolean;
+  close?: boolean;
+  isButton?: boolean;
+  handleClose?: () => void;
 }
 
-const TagCard = ({ _id, name, count, showCount, compact }: Props) => {
+const TagCard = ({
+  _id,
+  name,
+  count,
+  showCount,
+  compact,
+  close,
+  isButton,
+  handleClose,
+}: Props) => {
   const iconClass = getDeviconClassName(name);
 
-  console.log(compact);
+  const handleClick = (e: React.MouseEvent) => e.preventDefault();
 
-  return (
-    <Link
-      href={ROUTES.TAGS(_id)}
-      className="flex items-center justify-between gap-2"
-    >
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+  const CardContent = (
+    <>
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`} />
           <span>{name}</span>
         </div>
       </Badge>
 
+      {close && (
+        <Image
+          src="/icons/close.svg"
+          width={12}
+          height={12}
+          alt="close icon"
+          className="cursor-pointer object-contain invert-0 dark:invert"
+          onClick={handleClose}
+        />
+      )}
+
       {showCount && (
         <p className="small-medium text-dark500_light700">{count}</p>
       )}
-    </Link>
+    </>
   );
+
+  if (compact) {
+    if (isButton) {
+      return (
+        <button className="flex justify-between gap-2" onClick={handleClick}>
+          {CardContent}
+        </button>
+      );
+    } else {
+      return (
+        <Link
+          href={ROUTES.TAGS(_id)}
+          className="flex items-center justify-between gap-2"
+        >
+          {CardContent}
+        </Link>
+      );
+    }
+  }
 };
 
 export default TagCard;
