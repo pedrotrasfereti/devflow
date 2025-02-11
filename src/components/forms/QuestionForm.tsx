1d3e8d883c27589a2dc5e623baac139a8919de97
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-10 21:15:58 -0300

    feat: add QuestionForm component

---
+++ src/components/forms/QuestionForm.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import { AskQuestionSchema } from "@/lib/validations";
+
+import { Form } from "../ui/form";
+
+const QuestionForm = () => {
+  const form = useForm({
+    resolver: zodResolver(AskQuestionSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      tags: [],
+    },
+  });
+
+  const handleCreateQuestion = () => {};
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateQuestion)}
+      />
+    </Form>
+  );
+};
+
+export default QuestionForm;
