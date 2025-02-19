a14e4c65a76c7a75c42a5de42740043b0379a013
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-19 18:57:58 -0300

    feat(lib): add http error classes for error handling

---
+++ src/lib/http-errors.ts
@@ -0,0 +1,61 @@
+export class RequestError extends Error {
+  statusCode: number;
+  errors?: Record<string, string[]>;
+
+  constructor(
+    statusCode: number,
+    message: string,
+    errors?: Record<string, string[]>
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.name = "RequestError";
+  }
+}
+
+export class ValidationError extends RequestError {
+  constructor(fieldErrors: Record<string, string[]>) {
+    const message = ValidationError.formatFieldErrors(fieldErrors);
+    super(400, message, fieldErrors);
+    this.name = "ValidationError";
+    this.errors = fieldErrors;
+  }
+
+  static formatFieldErrors(errors: Record<string, string[]>): string {
+    const formattedMessages = Object.entries(errors).map(
+      ([field, messages]) => {
+        const fieldName = field.charAt(0).toUpperCase() + field.slice(1);
+
+        if (messages[0] === "Required") {
+          return `${fieldName} is required`;
+        } else {
+          return messages.join(" and ");
+        }
+      }
+    );
+
+    return formattedMessages.join(", ");
+  }
+}
+
+export class NotFoundError extends RequestError {
+  constructor(resource: string) {
+    super(404, `${resource} not found`);
+    this.name = "NotFoundError";
+  }
+}
+
+export class ForbiddenError extends RequestError {
+  constructor(resource: string) {
+    super(403, `${resource} not found`);
+    this.name = "ForbiddenError";
+  }
+}
+
+export class UnauthorizedError extends RequestError {
+  constructor(resource: string) {
+    super(401, `${resource} not found`);
+    this.name = "UnauthorizedError";
+  }
+}
