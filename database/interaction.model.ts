c9288f6d12c45d4653508af60a2e7f54a03316dd
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-19 16:40:47 -0300

    feat(db): create interaction model

---
+++ database/interaction.model.ts
@@ -0,0 +1,24 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IInteraction {
+  user: Schema.Types.ObjectId;
+  action: string;
+  actionId: Schema.Types.ObjectId;
+  actionType: "question" | "answer";
+}
+
+const InteractionSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  action: { type: String, required: true },
+  actionId: { type: Schema.Types.ObjectId, refPath: "type", required: true },
+  actionType: {
+    type: String,
+    required: true,
+    enum: ["Question", "Answer"],
+  },
+});
+
+const Interaction =
+  models?.interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
