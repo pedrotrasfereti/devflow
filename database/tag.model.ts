c7b9fafbbd391ba4af4dbaa6f1e46a23d04df1cb
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-19 15:53:16 -0300

    feat(db): create tag model

---
+++ database/tag.model.ts
@@ -0,0 +1,15 @@
+import { model, models, Schema } from "mongoose";
+
+export interface ITag {
+  name: string;
+  questions: number;
+}
+
+const TagSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  questions: { type: Number, default: 0 },
+});
+
+const Tag = models?.tag || model<ITag>("Tag", TagSchema);
+
+export default Tag;
