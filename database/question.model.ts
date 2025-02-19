290cea6c03152a0c434a16e8aac1d95f85797084
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-19 15:41:35 -0300

    feat(db): create question model

---
+++ database/question.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  author: Types.ObjectId;
+  title: string;
+  content: string;
+  tags: Types.ObjectId[];
+  views: number;
+  answers: number;
+  upvotes: number;
+  downvotes: number;
+}
+
+const QuestionSchema = new Schema(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Question =
+  models?.question || model<IQuestion>("Question", QuestionSchema);
+
+export default Question;
