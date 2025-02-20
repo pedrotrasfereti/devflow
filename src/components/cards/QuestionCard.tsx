b9d2d694bc4d7d8ec8be8a6959f79ffab183a58f
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-20 14:50:35 -0300

    feat: improve QuestionCard layout

--- src/components/cards/QuestionCard.tsx
+++ src/components/cards/QuestionCard.tsx
@@ -28,49 +28,47 @@ const QuestionCard = ({
             </h3>
           </Link>
         </div>
+      </div>
 
-        <div className="mt-3.5 flex w-full flex-wrap gap-2">
-          {tags.map((tag: Tag) => (
-            <TagCard key={tag._id} _id={tag._id} name={tag.name} compact />
-          ))}
-        </div>
+      <div className="mt-3.5 flex w-full flex-wrap gap-2">
+        {tags.map((tag: Tag) => (
+          <TagCard key={tag._id} _id={tag._id} name={tag.name} compact />
+        ))}
+      </div>
 
-        <div className="flex-between mt-6 w-full flex-wrap gap-3">
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl={author.image}
+          alt={author.name}
+          value={author.name}
+          title={`• asked ${getTimeStamp(createdAt)}`}
+          href={ROUTES.PROFILE(author._id)}
+          textStyles="body-medium text-dark400_light700"
+          isAuthor
+        />
+
+        <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
           <Metric
-            imgUrl={author.image}
-            alt={author.name}
-            value={author.name}
-            title={`∙ asked ${getTimeStamp(createdAt)}`}
-            href={ROUTES.PROFILE(author._id)}
-            textStyles="body-medium text-dark400_light700"
-            isAuthor
+            imgUrl="/icons/like.svg"
+            alt="like"
+            value={upvotes}
+            title=" Votes"
+            textStyles="small-medium text-dark400_light800"
           />
-
-          <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
-            <Metric
-              imgUrl="/icons/like.svg"
-              alt="like"
-              value={upvotes}
-              title=" Votes"
-              textStyles="small-medium text-dark400_light800"
-            />
-
-            <Metric
-              imgUrl="/icons/message.svg"
-              alt="answers"
-              value={answers}
-              title=" Answers"
-              textStyles="small-medium text-dark400_light800"
-            />
-
-            <Metric
-              imgUrl="/icons/eye.svg"
-              alt="views"
-              value={views}
-              title=" Views"
-              textStyles="small-medium text-dark400_light800"
-            />
-          </div>
+          <Metric
+            imgUrl="/icons/message.svg"
+            alt="answers"
+            value={answers}
+            title=" Answers"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/icons/eye.svg"
+            alt="views"
+            value={views}
+            title=" Views"
+            textStyles="small-medium text-dark400_light800"
+          />
         </div>
       </div>
     </div>
