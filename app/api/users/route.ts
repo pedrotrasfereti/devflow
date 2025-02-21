35b138d21750b9178f63b1ec5c3b6f358b37c1c9
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-21 19:03:38 -0300

    chore(backend): add comments to identify route urls

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,6 +7,7 @@ import dbConnect from "@/lib/mongoose";
 import { UserSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
 
+// GET /api/users
 export async function GET() {
   try {
     await dbConnect();
@@ -25,6 +26,7 @@ export async function GET() {
   }
 }
 
+// POST /api/users
 export async function POST(request: Request) {
   try {
     await dbConnect();
