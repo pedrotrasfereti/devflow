e1340c6e3712736a6de7a647bbca6ee47b2ecced
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-21 19:09:52 -0300

    feat(backend): add DELETE users/{id} route

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -35,3 +35,33 @@ export async function GET(
     return handleError(error, "api") as APIErrorResponse;
   }
 }
+
+// DELETE /api/users/{id}
+export async function DELETE(
+  request: Request,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
+
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+
+    // Same as `User.findOne({ _id: id });`
+    const user = await User.findByIdAndDelete(id);
+
+    // Check if user exists
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: user,
+      },
+      { status: 204 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
