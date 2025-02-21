66d088f5697693a44d0846b876fa1db172c8e00e
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-21 18:49:43 -0300

    feat(backend): add POST users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
 
 export async function GET() {
@@ -18,6 +20,45 @@ export async function GET() {
       },
       { status: 200 }
     );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    // Validate user data
+    const validatedData = UserSchema.safeParse(body);
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const user = validatedData.data;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email: user.email });
+
+    if (existingUser) {
+      throw new Error("User already exists.");
+    }
+
+    // Check if username is already taken
+    const existingUsername = await User.findOne({
+      username: user.username,
+    });
+
+    if (existingUsername) {
+      throw new Error("That username is taken. Try a different one.");
+    }
+
+    // Create user
+    const newUser = await User.create(validatedData.data);
+
+    return NextResponse.json({ success: true, data: newUser }, { status: 201 });
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }
