1d5e29052052a79e3411fcf03b6a7d6bd4edc492
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-22 17:25:12 -0300

    feat(backend): add PUT /account/{id} route

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -3,8 +3,9 @@ import { NextResponse } from "next/server";
 import Account from "@/database/account.model";
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
-import { NotFoundError } from "@/lib/http-errors";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
 
 // GET /api/account/{id}
@@ -42,6 +43,38 @@ export async function DELETE(
     if (!account) throw new NotFoundError("Account");
 
     return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// PUT /api/account/{id}
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const validatedData = AccountSchema.partial().safeParse(body);
+
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    const updatedAccount = await Account.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    });
+
+    if (!updatedAccount) throw new NotFoundError("Account");
+
+    return NextResponse.json(
+      { success: true, data: updatedAccount },
+      { status: 200 }
+    );
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }
