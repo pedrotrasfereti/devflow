871898a203bea0719c2fca33a45afcac7c0bbd8f
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-22 17:24:47 -0300

    feat(backend): add DELETE /account/{id} route

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 import Account from "@/database/account.model";
+import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
@@ -18,6 +19,26 @@ export async function GET(
     await dbConnect();
 
     const account = await Account.findById(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// DELETE /api/users/{id}
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const account = await User.findByIdAndDelete(id);
     if (!account) throw new NotFoundError("Account");
 
     return NextResponse.json({ success: true, data: account }, { status: 200 });
