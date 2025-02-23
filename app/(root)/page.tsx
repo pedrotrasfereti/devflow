965d45ea82f0db4fc966dd794109de66c98e2bc8
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-23 15:05:58 -0300

    feat(auth): implement session, jwt and sign in callbacks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
@@ -53,6 +54,10 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  const session = await auth();
+
+  console.log("SESSION: ", session);
+
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
