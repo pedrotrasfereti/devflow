c11d17925e633ef300356ad6bad5f77a33684ebd
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-23 14:42:08 -0300

    feat(lib/api): define oAuthSignIn call method

---
+++ types/action.d.ts
@@ -0,0 +1,10 @@
+interface OAuthSignIn {
+  provider: "github" | "google";
+  providerAccountId: string;
+  user: {
+    email: string;
+    name: string;
+    image: string;
+    username: string;
+  };
+}
