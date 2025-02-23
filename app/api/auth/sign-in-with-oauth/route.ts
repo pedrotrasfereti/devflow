121e43cf04a965dace163b31c9ed1ea4cc87cecb
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-23 14:35:32 -0300

    feat(backend): add POST /auth/sign-in-with-oauth route

---
+++ app/api/auth/sign-in-with-oauth/route.ts
@@ -0,0 +1,102 @@
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+import Account from "@/database/account.model";
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { SignInWithOAuthSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+// Sign in with oAuth
+export async function POST(request: Request) {
+  const { provider, providerAccountId, user } = await request.json();
+
+  await dbConnect();
+
+  const session = await mongoose.startSession();
+
+  /*
+    EXPLANATION: If any of the steps in a transaction fail, then the transaction is aborted altogether (A.K.A. an "atomic function"). If all steps succeed, then the transaction is commited and the changes are applied.
+  */
+  session.startTransaction();
+
+  try {
+    const validatedData = SignInWithOAuthSchema.safeParse({
+      provider,
+      providerAccountId,
+      user,
+    });
+
+    // Validate Sign In
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const { name, username, email, image } = user;
+
+    // Generate valid username
+    const formattedUsername = slugify(username, {
+      lower: true,
+      strict: true,
+      trim: true,
+    });
+
+    /*
+      .session() is used to assign the DB operation as part of the Mongoose transaction
+    */
+    let existingUser = await User.findOne({ email }).session(session);
+
+    if (!existingUser) {
+      [existingUser] = await User.create(
+        [{ name, username: formattedUsername, email, image }],
+        { session }
+      );
+    } else {
+      // Update user data with the newest oAuth Provider info
+      const updatedData: { name?: string; image?: string } = {};
+
+      if (existingUser.name !== name) updatedData.name = name;
+      if (existingUser.image !== image) updatedData.image = image;
+
+      // Check if changes are applicable
+      if (Object.keys(updatedData).length > 0) {
+        await User.updateOne(
+          { _id: existingUser._id },
+          { $set: updatedData }
+        ).session(session);
+      }
+    }
+
+    const existingAccount = await Account.findOne({
+      userId: existingUser._id,
+      provider,
+      providerAccountId,
+    }).session(session);
+
+    if (!existingAccount) {
+      await Account.create(
+        [
+          {
+            userId: existingUser._id,
+            name,
+            image,
+            provider,
+            providerAccountId,
+          },
+        ],
+        { session }
+      );
+    }
+
+    // Apply changes
+    await session.commitTransaction();
+  } catch (error: unknown) {
+    // Abort changes
+    await session.abortTransaction();
+    return handleError(error, "api") as APIErrorResponse;
+  } finally {
+    session.endSession();
+  }
+}
